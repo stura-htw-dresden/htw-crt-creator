6898ea0692b996766b7453b9fdecd904e0f561e2
PwnyTail <pwnytail@c3d2.de>
2015-11-26 14:04:45 +0100

    initial commit

---
+++ crt-creator.sh
@@ -0,0 +1,48 @@
+#! /bin/sh
+
+usage() 
+{
+    echo -e "$0 <Common Name> <eMail Address>";
+    echo -e "$0 -e <Common Name> <eMail Address>";
+    _exit 1;
+}
+
+_exit()
+{
+    if [ $1 -ne 0 ];
+    then
+        exit $1;
+    fi
+}
+
+if [ $# -lt 2 ];
+then
+    usage;
+elif [ $# -eq 2 ];
+then
+    CN=$1;
+    emailAddress=$2;
+elif [ $# -eq 3 ];
+then
+    ENCRYPT=$1
+    CN=$2;
+    emailAddress=$3;
+else
+    usage;
+fi
+
+if [ "${ENCRYPT}" = '-e' ];
+then
+    openssl genrsa -des3 -out ${CN}.encrypt.key 4096
+    _exit $?;
+else
+    openssl genrsa -out ${CN}.key 4096
+    _exit $?;
+fi
+
+openssl req -new -sha256 -subj \
+    "/CN=${CN}/emailAddress=${emailAddress}/O=Hochschule fuer Technik und Wirtschaft Dresden (FH)/L=Dresden/ST=Sachsen/C=DE/" \
+    -key ${CN}.key -out ${CN}.csr
+
+_exit $?;
+exit 0;
